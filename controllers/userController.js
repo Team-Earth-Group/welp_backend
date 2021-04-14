92316cc4e2d1e7980384b0efc40acb19b2b09a89
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-04-14 16:28:18 -0600

    added formatting

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,85 +5,85 @@ const userController = {}
 //signup
 userController.createUser = async (req, res) => {
     try {
-      const user = await models.user.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-      })
-      res.json({message: 'New user has been created', user})
+        const user = await models.user.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.json({ message: 'New user has been created', user })
 
     } catch (error) {
         res.status(400)
         res.json({ error: 'User already exists with this email' })
-      }
+    }
 }
 
 
 //login
 userController.login = async (req, res) => {
-    try{
+    try {
         const user = await models.user.findOne({
             where: {
                 email: req.body.email
             }
         })
-        if(user.password === req.body.password){
-            res.json({ message: 'login successful', user: user})
-        }else{
+        if (user.password === req.body.password) {
+            res.json({ message: 'login successful', user: user })
+        } else {
             res.status(401)
             res.json({ error: 'incorrect password' })
         }
-    }catch(error){
+    } catch (error) {
         res.status(400)
-        res.json({ error: 'login failed'})
+        res.json({ error: 'login failed' })
     }
 }
 
 //get all users
-userController.getAll = async (req,res) => {
-    try{
+userController.getAll = async (req, res) => {
+    try {
         console.log("hi")
         const users = await models.user.findAll()
         console.log(users);
         res.json({ users })
-    }catch (error){
+    } catch (error) {
         res.status(400)
-        res.json({ error: 'users info not found'})
+        res.json({ error: 'users info not found' })
     }
 }
 
 //user info
-userController.getUser = async (req,res) => {
-    try{
+userController.getUser = async (req, res) => {
+    try {
         const user = await models.user.findOne({
-            where:{
+            where: {
                 id: req.params.id
             }
         })
         res.json({ user })
-    }catch (error){
+    } catch (error) {
         res.status(400)
-        res.json({ error: 'user info not found'})
+        res.json({ error: 'user info not found' })
     }
 }
 
 //update user
 userController.update = async (req, res) => {
     try {
-      const user = await models.user.findOne({
-        where:{
-            id: req.params.id
-        }
-      })
-      const updatedUser = await user.update(req.body)
+        const user = await models.user.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+        const updatedUser = await user.update(req.body)
 
 
-      res.json({updatedUser})
+        res.json({ updatedUser })
     } catch (error) {
-      res.status(400)
-        res.json({ error: 'user could not be updated'})
+        res.status(400)
+        res.json({ error: 'user could not be updated' })
     }
-  }
+}
 
 //delete user
 userController.destroy = async (req, res) => {
@@ -94,30 +94,12 @@ userController.destroy = async (req, res) => {
                     id: req.params.id
                 }
             })
-            await user.destroy()
-            res.json({user, message: 'Your account has been deleted'})
-    }catch (error) {
+        await user.destroy()
+        res.json({ user, message: 'Your account has been deleted' })
+    } catch (error) {
         res.status(400)
-        res.json({ error: 'could not delete user'})
-        }
+        res.json({ error: 'could not delete user' })
+    }
 }
 module.exports = userController
 
-
-
-// // Find saved business info
-// userController.getbusiness = async (req, res) => {
-//     try {
-//       const user = await models.user.findOne({
-//         where:{
-//           userId: req.params.userId,
-//           businessId: req.params.businessId
-//         }
-//       })
-//       res.json({ business })
-
-//     } catch (error) {
-//         res.status(400)
-//         res.json({ error: 'could not save business information'})
-//     }
-//   }
