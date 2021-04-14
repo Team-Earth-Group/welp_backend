c79ba0f3c98f34d404ad225356826cea14e06e80
Ismail <ismail.desouki@gmail.com>
2021-04-14 12:59:29 -0700

    setup done

--- models/business.js
+++ models/business.js
@@ -11,6 +11,8 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      models.business.belongsTo(models.user)
+      models.business.hasMany(models.comment)
     }
   };
   business.init({
