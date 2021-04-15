8ae6f44b65d4ee715e5a3da00e6a726afb80acdd
Clement Mwimo <mwclemy@Clements-MacBook-Pro.local>
2021-04-15 09:32:32 -0600

    added crud for comment

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -21,5 +21,104 @@ businessController.getAll = async (req, res) => {
 
 }
 
+businessController.getComments = async (req, res) => {
+    try {
+        const business = await models.business.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        const comments = await business.getComments()
+        res.json({
+            comments
+        })
+    } catch (error) {
+        res.status(400)
+        res.json({
+            message: 'Comments can not be found.'
+        })
+    }
+}
+
+businessController.createComment = async (req, res) => {
+    try {
+        const business = await models.business.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        const comment = await models.comment.create({
+            text: req.body.text,
+            userId: req.body.userId
+        })
+
+        await business.addComment(comment)
+
+        res.json({
+            business,
+            comment
+        })
+    } catch (error) {
+        res.status(400)
+        res.json({
+            message: 'Comment can not be created.'
+        })
+    }
+}
+
+businessController.updateComment = async (req, res) => {
+    try {
+        const commentId = req.params.commentId
+        const businessId = req.params.id
+        await models.comment.update(req.body, {
+            where: {
+                id: commentId,
+                businessId: businessId,
+                userId: req.body.userId
+            }
+        })
+
+        const comment = await models.comment.findOne({
+            where: {
+                id: commentId
+            }
+        })
+
+        const business = await models.business.findOne({
+            where: {
+                id: businessId
+            }
+        })
+        res.json({
+            business,
+            comment
+        })
+    } catch (error) {
+        res.status(400)
+        res.json({
+            message: 'Can not updated the comment.'
+        })
+    }
+}
+
+businessController.deleteComment = async (req, res) => {
+    try {
+        const comment = await models.comment.findOne({
+            where: {
+                id: req.params.commentId,
+                businessId: req.params.id
+            }
+        })
+
+        await comment.destroy()
+        res.json({ message: 'Your comment has been deleted.', comment })
+    } catch (error) {
+        res.status(400)
+        res.json({ message: 'Could not delete your comment' })
+    }
+}
+
 
 module.exports = businessController
--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -3,5 +3,8 @@ const businessController = require('../controllers/businessController')
 const businessRoutes = express.Router()
 
 businessRoutes.get('/', businessController.getAll)
-
+businessRoutes.get('/:id/comments', businessController.getComments)
+businessRoutes.post('/:id/comments', businessController.createComment)
+businessRoutes.put('/:id/comments/:commentId', businessController.updateComment)
+businessRoutes.delete('/:id/comments/:commentId', businessController.deleteComment)
 module.exports = businessRoutes
